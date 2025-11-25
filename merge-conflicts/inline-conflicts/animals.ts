6ce64fa693d092ae38c98ec6230b0aad65b21fe3
John Caplinger <john@livefront.com>
2025-11-25 13:55:39 -0600

    resolved more merge conflicts

--- merge-conflicts/inline-conflicts/animals.ts
+++ merge-conflicts/inline-conflicts/animals.ts
@@ -1,11 +1,7 @@
 // Simple array with one animal per letter of the alphabet
 export const animalByLetter = [
     'Alligator',
-<<<<<<< HEAD
-    'Black Bear',
-=======
     'Brown Bear',
->>>>>>> main
     'Chicken',
     'Duck',
     'Elephant',
@@ -16,11 +12,8 @@ export const animalByLetter = [
     'Jaguar',
     'Kangaroo',
     'Lioness',
-<<<<<<< HEAD
     'Zonkey',
-=======
     'Ponkey',
->>>>>>> main
     'Newt',
     'Octopus',
     'Penguin',
